602bb365e253bd51192152c09eec5660941686d8
Oliver <oliver@zehentleitner.co>
2023-10-04 19:27:49 +0200

    1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from Cython.Build import cythonize
 # Working Directory
 print(f"CWD: {os. getcwd()}")
 current_directory = os.getcwd()
-files = os.listdir(current_directory+"/lucit_licensing_python")
+files = os.listdir(current_directory+"/..")
 
 for file in files:
     print(file)
