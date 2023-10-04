a879d2aefb8cc80a330604dd693c64edd89ee5af
Oliver <oliver@zehentleitner.co>
2023-10-05 01:07:08 +0200

    1.0.20

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
          'lucit_licensing_python/manager.py'],
         annotate=False),
     name='lucit-licensing-python',
-    version="1.0.19",
+    version="1.0.20",
     author="LUCIT Systems and Development",
     author_email='info@lucit.tech',
     url="https://github.com/LUCIT-Systems-and-Development/lucit-licensing-python",
